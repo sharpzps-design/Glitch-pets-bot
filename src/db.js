8bc8a0111b972f6d7b2e1d24b39f544c1a5ce004
sharpzps-design <sharpz.ps@gmail.com>
2025-09-23 13:59:32 +0100

    Update and rename db.js to src/db.js

--- src/db.js
+++ src/db.js
@@ -1,10 +1,13 @@
-// db.js - using pg
+// src/db.js
 import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  // Accept Supabase's SSL cert in hosted environments (Render)
   ssl: { rejectUnauthorized: false }
 });
 
-export default pool;
+export default {
+  query: (text, params) => pool.query(text, params)
+};
