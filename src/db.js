ba8c667f5faf5732555b71a71cfffa4c1636815c
sharpzps-design <sharpz.ps@gmail.com>
2025-09-23 18:43:26 +0100

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,15 +1,13 @@
 // src/db.js
-import pg from "pg";
-import dotenv from "dotenv";
-
-dotenv.config();
+import pg from 'pg';
 
 const { Pool } = pg;
 
+// Render automatically provides DATABASE_URL in environment variables
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false, // needed for Supabase/Render
+    rejectUnauthorized: false, // needed for Render PostgreSQL
   },
 });
 
