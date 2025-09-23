a1d89c26a46d8c119905d1bc18096180a4ccd302
sharpzps-design <sharpz.ps@gmail.com>
2025-09-23 14:29:22 +0100

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,6 +1,5 @@
 // src/db.js
 import pkg from "pg";
-import dns from "dns";
 const { Pool } = pkg;
 
 const connectionString = process.env.DATABASE_URL;
@@ -8,12 +7,11 @@ if (!connectionString) {
   throw new Error("DATABASE_URL is not set in environment variables");
 }
 
-// Force DNS resolution to IPv4
-dns.setDefaultResultOrder("ipv4first");
-
 const pool = new Pool({
   connectionString,
-  ssl: { rejectUnauthorized: false },
+  ssl: {
+    rejectUnauthorized: false, // ✅ accept Supabase self-signed cert
+  },
 });
 
 export default {
